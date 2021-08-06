45bc7cda7dcdf05ce1ccd0f07ab78a6256cb0ed2
Julie J <jeannet.julie@gmail.com>
2021-08-06 21:42:42 +0200

    styles for pagination

--- app/Views/crud_view.php
+++ app/Views/crud_view.php
@@ -72,3 +72,24 @@
 </body>
 
 </html>
+
+<style>
+.pagination li a
+{
+    position: relative;
+    display: block;
+    padding: .5rem .75rem;
+    margin-left: -1px;
+    line-height: 1.25;
+    color: #007bff;
+    background-color: #fff;
+    border: 1px solid #dee2e6;
+}
+
+.pagination li.active a {
+    z-index: 1;
+    color: #fff;
+    background-color: #007bff;
+    border-color: #007bff;
+}
+</style>
