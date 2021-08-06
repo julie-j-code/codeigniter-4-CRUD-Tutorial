3c1627a504e523e7ac7d35528b2ac5f9f7e26585
Julie J <jeannet.julie@gmail.com>
2021-08-06 18:20:00 +0200

    CodeIgniter 4 CRUD Tutorial - Initial Set Up

---
+++ app/Controllers/Crud.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\CrudModel;
+
+class Crud extends BaseController{
+    function index (){
+        echo "Hello, this is a test";
+    }
+}
+
+?>
