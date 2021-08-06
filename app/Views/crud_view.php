111bc725677341e1fe37a2864f491e91134f9726
Julie J <jeannet.julie@gmail.com>
2021-08-06 20:33:33 +0200

    initital html view : crud_view

---
+++ app/Views/crud_view.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+     content="width=device-width, initial-scale=1, user-scalable=yes">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>Codeigniter 4 Crud Application</title>
+</head>
+<body>
+    <div class="container">
+        
+        <h2 class="text-center mt-4 mb-4">Codeigniter 4 Crud Application</h2>
+
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col">Sample Data</div>
+                    <div class="col text-right">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Gender</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </table>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+
+    </div>
+ 
+</body>
+</html>
