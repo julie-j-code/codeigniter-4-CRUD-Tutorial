34eeef9ba6ef4c5c8838b042aa3de9f4e07ce353
Julie J <jeannet.julie@gmail.com>
2021-08-06 21:00:25 +0200

    Codeigniter 4 CRUD Tutorial - Fetch Data from Mysql Database with pagination links

--- app/Controllers/Crud.php
+++ app/Controllers/Crud.php
@@ -7,7 +7,12 @@ use App\Models\CrudModel;
 
 class Crud extends BaseController{
     function index (){
-        echo "Hello, this is a test";
+        // echo "Hello, this is a test";
+        $crudModel = new CrudModel();
+        $data['user_data']= $crudModel->orderBy('id', 'DESC')->paginate(10);
+        $data['pagination_link'] = $crudModel->pager;
+
+		return view('crud_view', $data);
     }
 }
 
--- app/Views/crud_view.php
+++ app/Views/crud_view.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport"
-     content="width=device-width, initial-scale=1, user-scalable=yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <title>Codeigniter 4 Crud Application</title>
 </head>
+
 <body>
     <div class="container">
-        
+
         <h2 class="text-center mt-4 mb-4">Codeigniter 4 Crud Application</h2>
 
         <div class="card">
@@ -17,7 +18,7 @@
                 <div class="row">
                     <div class="col">Sample Data</div>
                     <div class="col text-right">
-                        
+
                     </div>
                 </div>
             </div>
@@ -32,15 +33,42 @@
                             <th>Edit</th>
                             <th>Delete</th>
                         </tr>
+                        <?php
+
+                        if ($user_data) {
+                            foreach ($user_data as $user) {
+                                echo '
+                                <tr>
+                                    <td>' . $user["id"] . '</td>
+                                    <td>' . $user["name"] . '</td>
+                                    <td>' . $user["email"] . '</td>
+                                    <td>' . $user["gender"] . '</td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>';
+                            }
+                        }
+
+                        ?>
                     </table>
                 </div>
                 <div>
-                    
+                    <?php
+
+                    if ($pagination_link) {
+                        $pagination_link->setPath('crud');
+
+                        echo $pagination_link->links();
+                    }
+
+                    ?>
+
                 </div>
             </div>
         </div>
 
     </div>
- 
+
 </body>
+
 </html>
