ce49a0b2056f3f534c1d7efeaeb3867102a44dc9
Julie J <jeannet.julie@gmail.com>
2021-08-07 08:31:42 +0200

    Codeigniter 4 CRUD Tutorial - Insert or Add Data into Mysql Database with form data validation

--- app/Controllers/Crud.php
+++ app/Controllers/Crud.php
@@ -14,6 +14,40 @@ class Crud extends BaseController{
 
 		return view('crud_view', $data);
     }
-}
 
-?>
+    function add(){
+        return view('add_data');
+    }
+
+    function add_validation(){
+        helper(['form', 'url']);
+
+		$error = $this->validate([
+			'name'	=>	'required|min_length[3]',
+			'email'	=>	'required|valid_email',
+			'gender'=>	'required'
+		]);
+        if(!$error)
+		{
+			echo view('add_data', [
+				'error' 	=> $this->validator
+			]);
+		}
+		else
+		{
+			$crudModel = new CrudModel();
+
+			$crudModel->save([
+				'name'	=>	$this->request->getVar('name'),
+				'email'	=>	$this->request->getVar('email'),
+				'gender'=>	$this->request->getVar('gender')
+			]);
+
+			$session = \Config\Services::session();
+
+			$session->setFlashdata('success', 'User Data Added');
+
+			return $this->response->redirect(site_url('/crud'));
+		}
+    }
+}
--- app/Views/crud_view.php
+++ app/Views/crud_view.php
@@ -12,13 +12,24 @@
     <div class="container">
 
         <h2 class="text-center mt-4 mb-4">Codeigniter 4 Crud Application</h2>
+        <?php
+
+        $session = \Config\Services::session();
+
+        if ($session->getFlashdata('success')) {
+            echo '
+            <div class="alert alert-success">' . $session->getFlashdata("success") . '</div>
+            ';
+        }
+
+        ?>
 
         <div class="card">
             <div class="card-header">
                 <div class="row">
                     <div class="col">Sample Data</div>
                     <div class="col text-right">
-
+                    <a href="<?php echo base_url("/crud/add")?>" class="btn btn-success btn-sm">Create</a>
                     </div>
                 </div>
             </div>
@@ -74,22 +85,21 @@
 </html>
 
 <style>
-.pagination li a
-{
-    position: relative;
-    display: block;
-    padding: .5rem .75rem;
-    margin-left: -1px;
-    line-height: 1.25;
-    color: #007bff;
-    background-color: #fff;
-    border: 1px solid #dee2e6;
-}
+    .pagination li a {
+        position: relative;
+        display: block;
+        padding: .5rem .75rem;
+        margin-left: -1px;
+        line-height: 1.25;
+        color: #007bff;
+        background-color: #fff;
+        border: 1px solid #dee2e6;
+    }
 
-.pagination li.active a {
-    z-index: 1;
-    color: #fff;
-    background-color: #007bff;
-    border-color: #007bff;
-}
+    .pagination li.active a {
+        z-index: 1;
+        color: #fff;
+        background-color: #007bff;
+        border-color: #007bff;
+    }
 </style>
